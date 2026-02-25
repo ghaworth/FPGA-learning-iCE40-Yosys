ae7ecf8f1c6b23d72c8714615499a1aca2560f43
George Haworth <info@robust-ae.com>
2026-02-25 05:52:06 +0000

    Day 7: added LOAD and PROCESS to state machine

--- projects/aoc_2025/day_07/day_07_core.v
+++ projects/aoc_2025/day_07/day_07_core.v
@@ -38,19 +38,36 @@ module Day_07_Core (
 			bitshifts <= 0;
 			splits <= 0;
 			done <= 0;
+			data_valid <= 0;
 			state <= LOAD;
 		end
 
 		case(state)
 			LOAD: begin
-
+				if (!data_valid) begin
+					// wait one cycle for RAM read
+					data_valid <= 1;
+				end else begin
+					bitmask[words_read*16 +: 16] <= splitter_data;
+					words_read <= words_read + 1;
+					ram_addr <= ram_addr + 1;
+					data_valid <= 0;
+					if (words_read >= 8) begin
+						state <= PROCESS;
+					end
+				end
 			end
+
 			PROCESS: begin
-
+				hits <= active & bitmask;
+				active <= (active & ~bitmask) | ((active & bitmask) << 1) | ((active & bitmask) >> 1);
+				state <= COUNT;
 			end
+
 			COUNT: begin
 
 			end
+			
 			DONE: begin
 
 			end
