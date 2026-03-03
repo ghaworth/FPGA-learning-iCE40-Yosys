0b29b0a94d8bcbbd27490a9cfe46b6714a9ec8da
George Haworth <info@robust-ae.com>
2026-03-03 14:18:07 +0000

    CI: restrict to aoc_2025, clean up day_01 testbench

--- projects/aoc_2025/day_01/day_01_core_tb.v
+++ projects/aoc_2025/day_01/day_01_core_tb.v
@@ -23,15 +23,12 @@ module day_01_core_tb;
 		#10;
 		start = 0;
 
-		repeat(20) @(posedge clk);
-  		$display("DEBUG: running=%b index=%d data_valid=%b done=%b start=%b",
-    		uut.running, uut.index, uut.data_valid, uut.done, start);
-
+		@(posedge clk);
 		wait(done);	
 		if (result == 6860) begin
 			$display("PASS: result = %d", result);
 		end	else begin
-			$display("FAIL: expected 1158, got %d", result);
+			$display("FAIL: expected 6860, got %d", result);
 		end
 		$finish;
 	end
