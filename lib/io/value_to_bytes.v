7df2a0901b178e7edbb5bc6720bfc89c7fce0e0d
George Haworth <info@robust-ae.com>
2026-03-12 11:22:54 +0000

    lib/io: add parameterised value-to-bytes serialiser for UART transmission

---
+++ lib/io/value_to_bytes.v
@@ -0,0 +1,38 @@
+module Value_To_Bytes 
+    #(parameter VALUE_WIDTH)
+    (
+     input clk,
+     input send_data_now,
+     input next_byte_please,
+     input [VALUE_WIDTH-1:0] data_in,
+     output reg [7:0] byte_out = 0,
+     output reg byte_sent = 0,
+     output reg all_bytes_sent = 0
+     );
+
+    localparam NUM_BYTES = (VALUE_WIDTH + 7) / 8;
+    reg [VALUE_WIDTH-1:0] shift_reg = 0;
+    reg [NUM_BYTES+1:0] byte_index = 1;  
+
+    always @(posedge clk) begin
+        byte_sent <= 0;
+        if (send_data_now) begin
+            byte_index <= 1;
+            all_bytes_sent <= 0;
+            shift_reg <= data_in >> 8;
+            byte_out <= data_in[7:0];
+            byte_sent <= 1;
+
+        end else if (next_byte_please) begin
+            if (byte_index < NUM_BYTES) begin
+                shift_reg <= shift_reg >> 8;
+                byte_out <= shift_reg[7:0];
+                byte_index <= byte_index + 1;
+                byte_sent <= 1;
+            end else begin
+                all_bytes_sent <= 1;
+                byte_index <= 1; //reset for next time                
+            end
+        end
+    end
+endmodule
