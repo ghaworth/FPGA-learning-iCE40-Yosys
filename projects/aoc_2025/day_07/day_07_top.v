bfdfb6b8708976e394f1e1888c24898bee284dc4
George Haworth <info@robust-ae.com>
2026-03-13 07:18:33 +0000

    Day 7: UART output with muxed result and cycle count, timer module

--- projects/aoc_2025/day_07/day_07_top.v
+++ projects/aoc_2025/day_07/day_07_top.v
@@ -1,36 +1,69 @@
 module Day_07_Top (
   input i_Clk,
   input i_Switch_1,
-  output o_Segment1_A,
-  output o_Segment1_B,
-  output o_Segment1_C,
-  output o_Segment1_D,
-  output o_Segment1_E,
-  output o_Segment1_F,
-  output o_Segment1_G,
-  output o_Segment2_A,
-  output o_Segment2_B,
-  output o_Segment2_C,
-  output o_Segment2_D,
-  output o_Segment2_E,
-  output o_Segment2_F,
-  output o_Segment2_G,
-  output o_LED_1,
-  output o_LED_2,
-  output o_LED_3,
-  output o_LED_4
+  output o_UART_TX
 );
+  // state machine definitions
+  localparam IDLE = 0;
+  localparam SEND_RESULT = 1;
+  localparam SEND_TIMER = 2;
+  reg [2:0] current_state = 0;
+  wire sending_result = (current_state == SEND_RESULT);
+
+  reg [2:0] prev_state = 0;
+  wire enter_send_result = (current_state == SEND_RESULT) && (prev_state != SEND_RESULT);
+  wire enter_send_timer = (current_state == SEND_TIMER) && (prev_state != SEND_TIMER);
+
+  always @(posedge i_Clk) begin
+    prev_state <= current_state;
+    case (current_state) 
+      IDLE: begin 
+        if (core_done_edge) begin
+          current_state <= SEND_RESULT;
+        end
+      end  
+
+      SEND_RESULT: begin
+        if (result_done) begin
+          current_state <= SEND_TIMER;
+        end
+      end
+
+      SEND_TIMER: begin 
+        if (cc_done) begin
+          current_state <= IDLE;
+        end
+      end
+    endcase
+  end
+
+  // muxed signals
+  wire [7:0] mux_byte_out = sending_result ? result_out : cc_byte_out;
+  wire mux_byte_sent = sending_result ? result_sent : cc_sent;
+  wire result_next = sending_result ? tx_done : 1'b0;
+  wire cc_next = sending_result ? 1'b0 : tx_done;
+
   // core outputs
   wire [10:0] core_result;
   wire core_done;
 
-  // bcd converter outputs
-  wire [63:0] bcd_result;
-  wire bcd_done;
+  // timer outputs
+  wire [31:0] tmr_result;
+  wire tmr_done;
 
-  // scroll display outputs
-  wire [4:0] left_digit;
-  wire [4:0] right_digit;
+  // cycle count outputs
+  wire [7:0] cc_byte_out;
+  wire cc_sent;
+  wire cc_done;
+
+  // result outputs
+  wire [7:0] result_out;
+  wire result_sent;
+  wire result_done;
+
+  // uart transmitter outputs
+  wire tx_active;
+  wire tx_done;
 
   // debounced switch
   wire reset_debounced;
@@ -61,50 +94,41 @@ module Day_07_Top (
     .start(button_edge)
   ); 
 
-  bin_to_bcd b2bcd (
-    .start(core_done_edge),
-    .clk(i_Clk),
-    .bin_num(core_result),
-    .bcd_num(bcd_result),
-    .done(bcd_done)
+uart_tx #(.CLKS_PER_BIT(217)) uart_trans (
+    .i_Clock    (i_Clk),
+    .i_Tx_DV    (mux_byte_sent),
+    .i_Tx_Byte  (mux_byte_out),
+    .o_Tx_Active(tx_active),
+    .o_Tx_Serial(o_UART_TX),
+    .o_Tx_Done  (tx_done)
   );
 
-  scroll_display sdisp (
-    .clk(i_Clk),
-    .start(bcd_done),
-    .left_digit(left_digit),
-    .right_digit(right_digit),
-    .bcd_num(bcd_result),
-    .num_digits(5'd4)
+Day_07_Timer tmr (
+    .clk   (i_Clk),
+    .start (button_edge),
+    .stop  (core_done),
+    .result(tmr_result),
+    .done  (tmr_done)
   );
 
-  Binary_To_7Segment seg1 (
-    .i_Clk(i_Clk),
-    .i_Binary_Num(left_digit),
-    .o_Segment_A(o_Segment1_A),
-    .o_Segment_B(o_Segment1_B),
-    .o_Segment_C(o_Segment1_C),
-    .o_Segment_D(o_Segment1_D),
-    .o_Segment_E(o_Segment1_E),
-    .o_Segment_F(o_Segment1_F),
-    .o_Segment_G(o_Segment1_G)
-);
+Value_To_Bytes #(.VALUE_WIDTH(32)) cycle_count (
+    .clk             (i_Clk),
+    .send_data_now   (enter_send_timer),
+    .next_byte_please(cc_next),
+    .data_in         (tmr_result),
+    .byte_out        (cc_byte_out),
+    .byte_sent       (cc_sent),
+    .all_bytes_sent  (cc_done)
+  );
 
-Binary_To_7Segment seg2 (
-    .i_Clk(i_Clk),
-    .i_Binary_Num(right_digit),
-    .o_Segment_A(o_Segment2_A),
-    .o_Segment_B(o_Segment2_B),
-    .o_Segment_C(o_Segment2_C),
-    .o_Segment_D(o_Segment2_D),
-    .o_Segment_E(o_Segment2_E),
-    .o_Segment_F(o_Segment2_F),
-    .o_Segment_G(o_Segment2_G)
-);
-
-assign o_LED_1 = core_done;
-assign o_LED_2 = bcd_done;
-assign o_LED_3 = |left_digit;   // any bits set in left_digit
-assign o_LED_4 = reset_debounced;
+Value_To_Bytes #(.VALUE_WIDTH(16)) result (
+    .clk             (i_Clk),
+    .send_data_now   (enter_send_result),
+    .next_byte_please(result_next),
+    .data_in         (core_result),
+    .byte_out        (result_out),
+    .byte_sent       (result_sent),
+    .all_bytes_sent  (result_done)
+    );
 
 endmodule
